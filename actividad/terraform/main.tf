90cd1608c43d4e956c205ac843f7d3e9dc097976
Leandritonica92 <espindola.leandro@yahoo.com>
2024-08-27 22:59:32 -0300

    up

--- actividad/terraform/main.tf
+++ actividad/terraform/main.tf
@@ -1,6 +1,24 @@
 resource "aws_instance" "demolocalstack" {
     ami           = "ami-0e2ff28bfb72a4e45"
     instance_type = "t2.micro"
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup python -m SimpleHTTPServer 80 &
+    EOF
+    tags = {
+        Name = "educacionit"
+    }
+}
+
+resource "aws_instance" "demolocalstack-2" {
+    ami           = "ami-0e2ff28bfb72a4e45"
+    instance_type = "t2.micro"
+    user_data = <<-EOF
+        #!/bin/bash
+        echo "Hello, World" > index.html
+        nohup python -m SimpleHTTPServer 80 &
+    EOF
     tags = {
         Name = "educacionit"
     }
